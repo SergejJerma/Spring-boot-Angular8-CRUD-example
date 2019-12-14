01a3e892e213269cca724020e1a37f6dee547a76
Serzas <j_sergej@yahoo.com>
2019-12-14 21:56:28 +0200

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+import { SearchDeleteComponent } from './search-delete/search-delete.component';
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+  {path:"", redirectTo:"home", pathMatch:"full"},
+  {path:"home", component:HomeComponent},
+  {path:"register", component:RegistrationComponent},
+  {path:"search", component:SearchDeleteComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
